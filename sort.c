e187f7b37678ccbad2dd8bc5b8399bdc420eb2e2
athuathira055-boop <athuathira055@gmail.com>
2026-09-07 15:42:12 +0530

    completed

---
+++ sort.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+void main()
+{
+void read(int[],int);
+void print(int[],int);
+void sort(int[],int);
+void merge(int[],int[],int[],int,int);
+int a[20],b[20],c[40],n1,n2;
+printf("Number of element in first array[1-20]:");
+scanf("%d",&n1);
+read(a,n1);
+printf("Number of element in second array[1-20]:");
+scanf("%d",&n2);
+read(b,n2);
+sort(a,n1);
+sort(b,n2);
+merge(a,b,c,n1,n2);
+printf("first array sorted:\n");
+print(a,n1);
+printf("\n second array sorted:\n");
+print(b,n2);
+printf("\n merged array:\n");
+print(c,n1+n2);
+}
+void read(int ar[],int n)
+{
+int i;
+printf("Enter %d element:\n",n);
+for(i=0;i<n;i++)
+scanf("%d",&ar[i]);
+return;
+}
+void sort(int ar[],int n)
+{
+int i,j,temp;
+for(i=0;i<n-1;i++)
+for(j=i+1;j<n;j++)
+if(ar[i]>ar[j])
+{
+temp=ar[i];
+ar[i]=ar[j];
+ar[j]=temp;
+}
+return;
+}
+void merge(int ar1[],int ar2[],int ar3[],int n1,int n2)
+{
+int i=0,j=0,k=0;
+while(i<n1 && j<n2)
+if(ar1[i]<ar2[j])
+ar3[k++]=ar1[i++];
+else
+ar3[k++]=ar2[j++];
+while(i<n1)
+ar3[k++]=ar1[i++];
+while(j<n2)
+ar3[k++]=ar2[j++];
+return;
+}
+void print(int ar[],int n)
+{
+int i;
+for(i=0;i<n;i++)
+printf("%d \t",ar[i]);
+return;
+}
